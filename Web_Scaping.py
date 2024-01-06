0a487d8c36bce8546b365439ac7424d2f74a8915
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2024-01-06 16:30:00 +0330

    Write The Structure of Web Scraping

--- Web_Scaping.py
+++ Web_Scaping.py
@@ -5,4 +5,21 @@ URL = "https://www.truecar.com/used-cars-for-sale/listings/audi/"
 
 soup = BeautifulSoup(urlopen(URL), "html.parser")
 
-print(soup.text)
+cars_info = soup.find_all("div", {"class": "card-content order-3 vehicle-card-body"})
+
+car_list = []
+for car in cars_info:
+    name = car.find("span", {"class": "truncate"}).text
+    year = car.find("span", {"class": "vehicle-card-year text-xs"}).text
+    operation = car.find("div", {"data-test": "vehicleMileage"}).text
+    city = car.find("div", {"class": "vehicle-card-location mt-1 text-xs"}).text
+    price = car.find("span", {"data-test": "vehicleListingPriceAmount"}).text
+    car_list.append(name)
+    car_list.append(year)
+    car_list.append(operation)
+    car_list.append(city)
+    car_list.append(price)
+
+
+print(car_list)
+print(len(car_list))
