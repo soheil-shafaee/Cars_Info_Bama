05eb236bfe9bfe655f6a57351a3a758c5b6c21e1
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2024-01-06 16:43:53 +0330

    Get data from 4 pages of Truecar.com

--- Web_Scaping.py
+++ Web_Scaping.py
@@ -1,25 +1,25 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-URL = "https://www.truecar.com/used-cars-for-sale/listings/audi/"
+for n in range(1, 5):
+    URL = f"https://www.truecar.com/used-cars-for-sale/listings/audi/?page={n}"
 
-soup = BeautifulSoup(urlopen(URL), "html.parser")
+    soup = BeautifulSoup(urlopen(URL), "html.parser")
 
-cars_info = soup.find_all("div", {"class": "card-content order-3 vehicle-card-body"})
+    cars_info = soup.find_all("div", {"class": "card-content order-3 vehicle-card-body"})
 
-car_list = []
-for car in cars_info:
-    name = car.find("span", {"class": "truncate"}).text
-    year = car.find("span", {"class": "vehicle-card-year text-xs"}).text
-    operation = car.find("div", {"data-test": "vehicleMileage"}).text
-    city = car.find("div", {"class": "vehicle-card-location mt-1 text-xs"}).text
-    price = car.find("span", {"data-test": "vehicleListingPriceAmount"}).text
-    car_list.append(name)
-    car_list.append(year)
-    car_list.append(operation)
-    car_list.append(city)
-    car_list.append(price)
-
-
-print(car_list)
-print(len(car_list))
+    car_list = []
+    for car in cars_info:
+        name = car.find("span", {"class": "truncate"}).text
+        year = car.find("span", {"class": "vehicle-card-year text-xs"}).text
+        operation = car.find("div", {"data-test": "vehicleMileage"}).text
+        city = car.find("div", {"class": "vehicle-card-location mt-1 text-xs"}).text
+        price = car.find("span", {"data-test": "vehicleListingPriceAmount"}).text
+        car_list.append(name)
+        car_list.append(year)
+        car_list.append(operation)
+        car_list.append(city)
+        car_list.append(price)
+    print("page: ", n)
+    print(car_list)
+    print(len(car_list))
