e21ab17ab5d072bc4583f91e873e8bd81b6298de
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2024-01-06 17:32:46 +0330

    INSERT cars inforamtion INTO cars_info TABLE

--- Web_Scaping.py
+++ Web_Scaping.py
@@ -1,5 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pymysql
+
+conx = pymysql.connect(host="localhost",
+                       user="root",
+                       password="",
+                       database="cars_information")
+
+cur = conx.cursor()
 
 for n in range(1, 5):
     URL = f"https://www.truecar.com/used-cars-for-sale/listings/audi/?page={n}"
@@ -11,15 +19,25 @@ for n in range(1, 5):
     car_list = []
     for car in cars_info:
         name = car.find("span", {"class": "truncate"}).text
-        year = car.find("span", {"class": "vehicle-card-year text-xs"}).text
-        operation = car.find("div", {"data-test": "vehicleMileage"}).text
+        year = int(car.find("span", {"class": "vehicle-card-year text-xs"}).text)
+        operation = car.find("div", {"data-test": "vehicleMileage"}).text.split()[0].split(",")
+        int_operation = int("".join(operation))
         city = car.find("div", {"class": "vehicle-card-location mt-1 text-xs"}).text
-        price = car.find("span", {"data-test": "vehicleListingPriceAmount"}).text
-        car_list.append(name)
-        car_list.append(year)
-        car_list.append(operation)
-        car_list.append(city)
-        car_list.append(price)
-    print("page: ", n)
-    print(car_list)
-    print(len(car_list))
+        price = car.find("span", {"data-test": "vehicleListingPriceAmount"}).text.split("$")[1].split(",")
+        int_price = int("".join(price))
+        sql = (name, year, int_operation, city, int_price)
+        com = """
+        INSERT INTO cars_info
+        VALUES(%s, %i, %i, %s, %i)
+        """ % sql
+
+        try:
+            cur.execute(com)
+            conx.commit()
+
+        except Exception as e:
+            print(e)
+            conx.rollback()
+
+        conx.close()
+
